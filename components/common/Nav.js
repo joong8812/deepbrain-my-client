d694c438993da7511c06f80d7fd437a72466cdfb
jungjaechoi <joong8812@gmail.com>
2022-04-22 00:57:51 +0900

    fix - nav camping site name

--- components/common/Nav.js
+++ components/common/Nav.js
@@ -6,7 +6,7 @@ export default function Nav(){
     <nav className={styles.nav}>
       <ul>
         <li className={styles.li}><Link href="/">메인</Link></li>
-        <li className={styles.li}><Link href="/board/board-list">게시글목록</Link></li>
+        <li className={styles.li}><Link href="/board/board-list">캠핑장 리스트</Link></li>
         <li className={styles.li}><Link href="/user/login">로그인</Link></li>
         <li className={styles.li}><Link href="/user/join">회원가입</Link></li>
       </ul>
